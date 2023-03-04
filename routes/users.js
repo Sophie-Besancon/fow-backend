9da401249a0efdfe30d204d4408198ded0395af6
Cyrila54 <cyril.a54@icloud.com>
2023-03-04 19:24:20 +0100

    cyril

--- routes/users.js
+++ routes/users.js
@@ -1,149 +1,175 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-require('../models/connection');
-const User = require('../models/users');
-const { checkBody } = require('../modules/checkBody');
-const uid2 = require('uid2');
-const bcrypt = require('bcrypt');
+require("../models/connection");
+const User = require("../models/users");
+const { checkBody } = require("../modules/checkBody");
+const uid2 = require("uid2");
+const bcrypt = require("bcrypt");
 
-router.post('/signup', (req, res) => {
-  if (!checkBody(req.body, ['lastname', 'password'])) {
-    res.json({ result: false, error: 'Champs vides ou manquants' });
+router.post("/signup", (req, res) => {
+  if (!checkBody(req.body, ["lastname", "password"])) {
+    res.json({ result: false, error: "Champs vides ou manquants" });
     return;
   }
 
   // Vérification si l'utilisateur est déjà existant
-  User.findOne({ lastname: req.body.lastname }).then(data => {
-    const regex = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g)
-    const mailTest = regex.test(req.body.mail)
-    
-    if (data === null) {
+  User.findOne({ lastname: req.body.lastname }).then((data) => {
+    const regex = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g);
+    const mailTest = regex.test(req.body.mail);
 
+    if (data === null) {
       const hash = bcrypt.hashSync(req.body.password, 10);
       const newUser = new User({
-        firstname:req.body.firstname,
-        lastname:req.body.lastname,
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
         password: hash,
-        mail:req.body.mail,
+        mail: req.body.mail,
         token: uid2(32),
         canBookmark: true,
       });
 
-      newUser.save().then(data => {
-        res.json({ result: true, data:data });
+      newUser.save().then((data) => {
+        res.json({ result: true, data: data });
       });
-    } else if (!mailTest){
-      res.json({ result: false, error: '❗️Votre adresse mail est incorrecte' });
-    } 
-
-    else {
+    } else if (!mailTest) {
+      res.json({
+        result: false,
+        error: "❗️Votre adresse mail est incorrecte",
+      });
+    } else {
       // utilisateur déjà existant dans la bdd
-      res.json({ result: false, error: 'Cet utilisateur existe déjà' });
+      res.json({ result: false, error: "Cet utilisateur existe déjà" });
     }
   });
 });
 
-router.post('/signin', (req, res) => {
-  if (!checkBody(req.body, ['mail', 'password'])) {
-    res.json({ result: false, error: 'Champs vides ou manquants' });
+router.post("/signin", (req, res) => {
+  if (!checkBody(req.body, ["mail", "password"])) {
+    res.json({ result: false, error: "Champs vides ou manquants" });
     return;
   }
 
-  User.findOne({ mail: req.body.mail }).then(data => {
+  User.findOne({ mail: req.body.mail }).then((data) => {
     if (data && bcrypt.compareSync(req.body.password, data.password)) {
       res.json({ result: true, data: data });
     } else {
-      res.json({ result: false, error: 'Utilisateur ou mot de passe incorrect' });
+      res.json({
+        result: false,
+        error: "Utilisateur ou mot de passe incorrect",
+      });
     }
   });
 });
 
-router.get('/infos/:token', (req, res) => {
-  User.findOne({ token: req.params.token }).then(data => {
+router.get("/infos/:token", (req, res) => {
+  User.findOne({ token: req.params.token }).then((data) => {
     if (data) {
       res.json({ result: true, data: data });
     } else {
-      res.json({ result: false, error: 'Utilisateur non trouvé' });
+      res.json({ result: false, error: "Utilisateur non trouvé" });
     }
   });
 });
 
-//route PUT pour enregistrer l'état isLiked de chaque article dans la BDD 
- router.put('/token', (req, res) => {
-  User.findOne({token: req.body.token}).then(data=>{
-      if(!data){
-      res.json({result: false, error: 'Utilisateur non trouvé'})
+//route PUT pour enregistrer l'état isLiked de chaque article dans la BDD
+router.put("/token", (req, res) => {
+  User.findOne({ token: req.body.token }).then((data) => {
+    if (!data) {
+      res.json({ result: false, error: "Utilisateur non trouvé" });
+    } else {
+      let articleArray = data.articlesinFavorite;
+      if (articleArray.includes(req.body.articleId)) {
+        articleArray = articleArray.filter(
+          (article) => article.toString() !== req.body.articleId
+        );
       } else {
-        let articleArray = data.articlesinFavorite;
-        if (articleArray.includes(req.body.articleId)){
-         articleArray = articleArray.filter(article => article.toString() !== req.body.articleId)
-        } else {
-          articleArray.push(req.body.articleId)
-        }
-        User.findOneAndUpdate({token: req.body.token}, {articlesinFavorite: articleArray})
-         .then(() => {
-          User.findOne({token: req.body.token}) 
-          .populate('articlesinFavorite')
-          .then(data=>{
-            res.json({result: true, data})
-          })
-         })   
+        articleArray.push(req.body.articleId);
       }
-  })
-}) 
+      User.findOneAndUpdate(
+        { token: req.body.token },
+        { articlesinFavorite: articleArray }
+      ).then(() => {
+        User.findOne({ token: req.body.token })
+          .populate("articlesinFavorite")
+          .then((data) => {
+            res.json({ result: true, data });
+          });
+      });
+    }
+  });
+});
 
+// route PUT qui permet de mettre a jour les données utilisateur
+router.put("/update/:token", (req, res) => {
+  const regex = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g);
+  const mailTest = regex.test(req.body.mail);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-router.put('/update/:token', (req,res)=>{
-  const regex = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g)
-  const mailTest = regex.test(req.body.mail)
-
-  if (req.body.password !=null){
-     var hash = bcrypt.hashSync(req.body.password, 10);
-}
-  if (req.body.firstname!='' && req.body.lastname!='' && mailTest){
-    User.findOneAndUpdate({ token: req.params.token },{firstname:req.body.firstname, lastname:req.body.lastname, mail:req.body.mail, password:hash})
-    .then(data => {
+  if (req.body.password != null) {
+    var hash = bcrypt.hashSync(req.body.password, 10);
+  }
+  if (req.body.firstname != "" && req.body.lastname != "" && mailTest) {
+    User.findOneAndUpdate(
+      { token: req.params.token },
+      {
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        mail: req.body.mail,
+        password: hash,
+      }
+    ).then((data) => {
       if (data) {
-        res.json({ result: true, data:data, message:'✅ Toutes est bien enregistré !' });
+        res.json({
+          result: true,
+          data: data,
+          message: "✅ Toutes est bien enregistré !",
+        });
       } else {
-        res.json({ result: false, message: '❌ Utilisateur non trouvé' });
+        res.json({ result: false, message: "❌ Utilisateur non trouvé" });
       }
     });
+  } else {
+    res.json({
+      result: false,
+      message: "❗️Adresse mail incorrecte ou champs manquants",
+    });
   }
-  else{
-    res.json({ result: false, message: '❗️Adresse mail incorrecte ou champs manquants' });
-  }
+});
 
-})
+//route POST qui permet d'ajouter une nouvelle adresse à un utilisateur
+router.post("/add_address/:token", (req, res) => {
+  const dataCity = {
+    street: req.body.street,
+    city: req.body.city,
+    zipCode: req.body.zipCode,
+    country: req.body.country,
+    isBillingAddress: req.body.isBillingAddress,
+    isDeliveryAddress: req.body.isDeliveryAddress,
+  };
+
+  User.findOne({ token: req.params.token }).then((dataFind) => {
+   
+      if (req.body.isBillingAddress) {
+
+          for (const element of dataFind.address){
+
+            console.log(element);
+          }
 
 
+      }
+    
 
-
+    dataFind.address.push({ ...dataCity });
+    User.findOneAndUpdate(
+      { token: req.params.token },
+      {
+        address: dataFind.address,
+      }
+    ).then((dataUpdate) => {
+      res.json({ result: true, message: "OK", data: dataUpdate });
+    });
+  });
+});
 
 module.exports = router;
